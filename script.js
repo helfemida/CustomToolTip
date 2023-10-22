147747e1ea43aa0d03e62ac78c917f4dd6e7d3de
Albina Yermek <yermekalbina@gmail.com>
2023-10-22 14:47:27 +0600

    toolti working properly

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// script.js
 
 const tooltipContainer = document.querySelector('.tooltip-container');
 const customTooltip = document.getElementById('custom-tooltip');
+var cursor = document.getElementById('cursor-bg');
 
 tooltipContainer.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('element')) {
         const tooltipText = e.target.getAttribute('data-tooltip');
         customTooltip.textContent = tooltipText;
         customTooltip.style.display = 'block';
-        customTooltip.style.left = e.target.offsetLeft + 'px';
-        customTooltip.style.top = e.target.offsetTop + e.target.offsetHeight + 'px';
+        
+        customTooltip.style.top = e.pageY + 'px';
+        customTooltip.style.left = e.pageX + 'px';
+
     }
 });
 
+
+
 tooltipContainer.addEventListener('mouseout', () => {
     customTooltip.style.display = 'none';
+
 });
